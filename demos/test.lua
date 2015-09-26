485973c828bab6e985d44e7383e3a825331b5bae
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-26 20:07:35 +0300

    Minor cleanup in demos test.

--- demos/test.lua
+++ demos/test.lua
@@ -1,23 +1,24 @@
-local pp = require 'core.pretty'
+--local pp = require 'core.pretty'
 local mixin = require 'core.mixin'
 local binder = require 'validation.binder'
 local required = require 'validation.rules.required'
 local validator = require 'validation.validator'
-
 local http = require 'http'
+
 
 mixin(http.Request, http.mixins.routing)
 mixin(http.ResponseWriter, http.mixins.json)
 
 local app = http.app.new()
 
+-- middleware
+
 app:use(function(options, following)
     return function(w, req)
         return following(w, req)
     end
 end)
-app:use(require 'http.middleware.routing')
-
+app:use(http.middleware.routing)
 
 -- decorators
 
@@ -39,8 +40,7 @@ local greeting_validator = validator.new({
     author = {required}
 })
 
-app
-:get('', function(w, req)
+app:get('', authorize, function(w, req)
     return w:write('Hello, world!\n')
 end)
 :post(authorize, check, function(w, req)
@@ -52,7 +52,10 @@ end)
         return w:json(b.errors, 400)
     end
 
-    return w:json({message='Hello World!'})
+    return w:json({
+        message='Hello World!',
+        path=req:absolute_url_for('user', {name='jack'})
+    })
 end)
 
 app:get('user/{name}', 'user', function(w, req)
