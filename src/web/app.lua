5347d004f5ac1f5776e9c92ba37b05fb00c9c980
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-28 15:52:07 +0300

    Refactoring web app middleware composition.

--- src/web/app.lua
+++ src/web/app.lua
@@ -1,18 +1,16 @@
 _ENV = nil
 
-local function wrap_middleware(following, func)
-    return function(w, req)
-        return func(w, req, following)
-    end
-end
-
-return function(middlewares, options)
-    local r, m
-    for i = #middlewares, 1, -1 do
-        m = middlewares[i](options)
-        if m then
-            r = wrap_middleware(r, m)
+--[[
+    function middleware(options, following)
+        return function(w, req)
+            following(w, req)
         end
     end
-    return r
+--]]
+return function(middlewares, options)
+    local following
+    for i = #middlewares, 1, -1 do
+        following = middlewares[i](options, following)
+    end
+    return following
 end
