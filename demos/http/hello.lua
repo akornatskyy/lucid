85e87add559a509a70aebff642023fdb51994b2b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-02 14:30:40 +0300

    Introduced http hello demo.

---
+++ demos/http/hello.lua
@@ -0,0 +1,14 @@
+local http = require 'http'
+
+local app = http.app.new()
+app:use(http.middleware.routing)
+
+--[[
+    lurl -v demos.http.redirect /
+    curl -v http://127.0.0.1:8080
+--]]
+app:get('', function(w, req)
+    return w:write('Hello World!\n')
+end)
+
+return app()
