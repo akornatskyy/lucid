f88c86219b17ad5232c785e412b1ecac9fe5cbee
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-08 20:10:14 +0300

    Remove auth cookie if writer principal is undefined in authcookie http middleware.

--- src/http/middleware/authcookie.lua
+++ src/http/middleware/authcookie.lua
@@ -1,22 +1,20 @@
-local dump
-do
-    local cookie = require 'http.cookie'
-    dump = cookie.dump
-end
-
 return function(following, options)
     assert(options.ticket, 'options.ticket')
     local name, path, domain, secure
+    local dump, deleted
     local ticket = options.ticket
     local principal
 
     do
+        local cookie = require 'http.cookie'
         local c = options.auth_cookie or {}
+        dump = cookie.dump
         name = c.name or '_a'
         path = c.path or options.root_path or '/'
         domain = c.domain
         secure = c.secure
         principal = options.principal or require 'security.principal'
+        deleted = cookie.delete {name = name, path = path}
     end
 
     return function(w, req)
@@ -31,6 +29,8 @@ return function(following, options)
                 secure=secure,
                 http_only=true
             }))
+        else
+            w:add_header('Set-Cookie', deleted)
         end
     end
 end
