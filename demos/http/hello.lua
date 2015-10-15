cacf797d3a111d247b433b032c84ab1a664bffa5
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-15 14:08:49 +0300

    Fixed misprint.

--- demos/http/hello.lua
+++ demos/http/hello.lua
@@ -4,7 +4,7 @@ local app = http.app.new()
 app:use(http.middleware.routing)
 
 --[[
-    lurl -v demos.http.redirect /
+    lurl -v demos.http.hello /
     curl -v http://127.0.0.1:8080
 --]]
 app:get('', function(w, req)
