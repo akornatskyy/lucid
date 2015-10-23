276207dbb21ea085e26d0fbc6befbbf9e371979b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-23 13:55:51 +0300

    Introduced request key module.

---
+++ spec/http/request_key_spec.lua
@@ -0,0 +1,92 @@
+local request_key = require 'http.request_key'
+local describe, it, assert = describe, it, assert
+
+describe('http.request_key', function()
+    it('raises an error if a variable is unknown', function()
+        assert.error(function()
+		    request_key.new('-$z-')
+        end, 'unknown variable "z"')
+	end)
+
+    it('raises an error if expression is not a string', function()
+        assert.error(function()
+		    request_key.new()
+        end)
+
+        assert.error(function()
+		    request_key.new('')
+        end)
+	end)
+
+    describe('expression', function()
+        local cases = {
+            ['key'] = {'key', nil},
+            ['GET'] = {'$m', {method='GET'}},
+            ['P-GET'] = {'P-$m', {method='GET'}},
+            ['GET-S'] = {'$m-S', {method='GET'}},
+            ['GET/'] = {'$m$p', {method='GET', path='/'}},
+            ['GET:/search/posts'] = {
+                '$m:$p',
+                {
+                    method='GET',
+                    path='/search/posts'
+                }
+            },
+            ['GET:/search/posts::'] = {
+                '$m:$p:$q_q:$q_page',
+                {
+                    method='GET', path='/search/posts',
+                    query={}
+                }
+            },
+            ['GET:/search/posts:vol:'] = {
+                '$m:$p:$q_q:$q_page',
+                {
+                    method='GET', path='/search/posts',
+                    query={q='vol'}
+                }
+            },
+            ['GET:/search/posts:vol:10'] = {
+                '$m:$p:$q_q:$q_page',
+                {
+                    method='GET', path='/search/posts',
+                    query={q='vol', page='10'}
+                }
+            },
+            ['GET::/'] = {
+                '$m:$h_accept_encoding:$p',
+                {
+                    method='GET', path='/',
+                    headers={}
+                }
+            },
+            ['GET:gzip, deflate:/'] = {
+                '$m:$h_accept_encoding:$p',
+                {
+                    method='GET', path='/',
+                    headers={['accept-encoding']='gzip, deflate'}
+                }
+            },
+            ['GET:/'] = {
+                '$m$gz:$p',
+                {
+                    method='GET', path='/', headers={}
+                }
+            },
+            ['GETz:/'] = {
+                '$m$gz:$p',
+                {
+                    method='GET', path='/',
+                    headers={['accept-encoding']='gzip, deflate'}
+                }
+            },
+        }
+        for r, c in next, cases do
+            local expression, req = unpack(c)
+            it(expression, function()
+                local make_key = request_key.new(expression)
+                assert.equals(r, make_key(req))
+            end)
+        end
+    end)
+end)
---
+++ src/http/request_key.lua
@@ -0,0 +1,123 @@
+local assert, format, load = assert, string.format, loadstring
+local concat = table.concat
+
+
+local function var_method()
+    return nil, 'req.method'
+end
+
+local function var_path()
+    return nil, 'req.path'
+end
+
+local function var_query(name)
+    return 'local q = req.query or req:parse_query()',
+           '(q["' .. name .. '"] or "")'
+end
+
+local function var_headers(name)
+    name = name:gsub('_', '-')
+    return 'local h = req.headers or req:parse_headers()',
+           '(h["' .. name .. '"] or "")'
+end
+
+local function var_gzip()
+    return 'local h = req.headers',
+        '((h["accept-encoding"] or ""):find("gzip", 1, true) and "z" or "")'
+end
+
+local variables = {
+    method = var_method,
+    m = var_method,
+    path = var_path,
+    p = var_path,
+    query = var_query,
+    q = var_query,
+    headers = var_headers,
+    header = var_headers,
+    h = var_headers,
+    gzip = var_gzip,
+    gz = var_gzip
+}
+
+local function keys(t)
+    local r = {}
+    for k in pairs(t) do
+        r[#r+1] = k
+    end
+    return r
+end
+
+local function parse_parts(s)
+    local parts = {}
+    local b = 1
+    local e = 1
+    local pe = 1
+    while true do
+        b, e = s:find('%$%w+', b)
+        if not b then
+            parts[#parts+1] = s:sub(pe)
+            break
+        end
+        parts[#parts+1] = s:sub(pe, b - 1)
+        local name = s:sub(b + 1, e)
+        local f = variables[name]
+        e = e + 1
+        b, pe = s:find('^_[%w_]+', e)
+        if b then
+            parts[#parts+1] = {name, s:sub(b + 1, pe)}
+            pe = pe + 1
+        else
+            parts[#parts+1] = {name}
+            pe = e
+        end
+        b = e
+    end
+    return parts
+end
+
+local function build(parts)
+    local locals = {}
+    local chunks = {}
+    for i = 1, #parts do
+        if i % 2 == 1 then
+            local s = parts[i]
+            if #s > 0 then
+                chunks[#chunks+1] = format('%q', s)
+            end
+        else
+            local name, param = unpack(parts[i])
+            local v = variables[name]
+            if not v then
+                return error('unknown variable "' .. name .. '"')
+            end
+            local l, c = v(param)
+            if l and #l > 0 then
+                locals[l] = true
+            end
+            chunks[#chunks+1] = c
+        end
+    end
+    locals = concat(keys(locals), '\n        ')
+    chunks = concat(chunks, ' ..\n            ')
+    return format([[
+
+    return function(req)
+        %s
+        return %s
+    end]], locals, chunks)
+end
+
+local function new(s)
+    if type(s) ~= 'string' or #s == 0 then
+        return error('bad argument #1 to \'new\' (non empty string expected)')
+    end
+    local source = build(parse_parts(s))
+    -- print(source)
+    return assert(load(source))()
+end
+
+return {
+    new = new,
+    variables = variables
+}
