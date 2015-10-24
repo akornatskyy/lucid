caec9e9a344106914064b7c76d4c820f6fbadf98
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-24 17:43:11 +0300

    Introduced http caching middleware.

---
+++ src/http/middleware/caching.lua
@@ -0,0 +1,60 @@
+local request_key = require 'http.request_key'
+local concat, next = table.concat, next
+
+
+local function pack(w)
+    local status_code = w:get_status_code()
+    local headers = w.headers
+    local r = {
+        b = concat(w.b)
+    }
+    if status_code ~= 0 and status_code ~= 200 then
+        r.s = status_code
+    end
+    if next(headers) ~= nil then
+        r.h = headers
+    end
+    return r
+end
+
+local function unpack(w, r)
+    local status_code = r.s
+    local headers = r.h
+    local body = r.b
+    if status_code then
+        w:set_status_code(status_code)
+    end
+    if headers then
+        w.headers = headers
+    end
+    w.b = body
+    w.s = #body
+end
+
+return function(options, following)
+    local cache = options.cache
+    assert(cache, 'cache')
+    local make_key = request_key.new '$m:$p'
+    local profiles = {}
+    return function(w, req)
+        local key
+        local mkey = make_key(req)
+        local mprofile = profiles[mkey]
+        if mprofile then
+            key = mprofile.key(req)
+            local r = cache:get(key)
+            if r then
+                return unpack(w, r)
+            end
+        end
+        following(w, req)
+        local profile = w.cache_profile
+        if profile then
+            if profile ~= mprofile then
+                profiles[mkey] = profile
+                key = profile.key(req)
+            end
+            cache:set(key, pack(w), profile.time)
+        end
+    end
+end
