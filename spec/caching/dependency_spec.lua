b7fe62d162913dd5bdfaf896837eb9487431d1d0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-27 22:22:16 +0200

    Implementation for cache dependency.

---
+++ spec/caching/dependency_spec.lua
@@ -0,0 +1,8 @@
+local dependency = require 'caching.dependency'
+local describe, it, assert = describe, it, assert
+
+describe('caching.dependency', function()
+	it('', function()
+		assert.not_nil(dependency)
+	end)
+end)
--- src/caching/dependency.lua
+++ src/caching/dependency.lua
@@ -3,13 +3,42 @@ local setmetatable = setmetatable
 local Dependency = {}
 local mt = {__index=Dependency}
 
+local function get_keys(self, master_key)
+    local n = self.cache:get(master_key)
+    if not n then
+        return nil
+    end
+    local keys = {}
+    for i=1, n do
+        keys[i] = master_key .. tostring(i)
+    end
+    local keys2 = self.cache:get_multi(keys)
+    for i=1, #keys2 do
+        keys[#keys+1] = keys2[i]
+    end
+    return keys
+end
+
 function Dependency:next_key(master_key)
+    local i = self.cache:incr(master_key)
+    if not i then
+        self.cache.add(master_key, 0)
+    end
+    i = self.cache:incr(master_key)
+    return master_key .. tostring(i)
 end
 
 function Dependency:add(master_key, key)
+    return self.cache:add(self:next_key(master_key), key, self.time)
 end
 
 function Dependency:delete(master_key)
+    local keys = get_keys(self, master_key)
+    if keys then
+        for i=1, #keys do
+            self.cache.delete(keys[i])
+        end
+    end
 end
 
 local function new(cache, time)
