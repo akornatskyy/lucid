1c7390ef0345ef4ab373b9861bd060de994c5124
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-11-07 21:54:43 +0200

    Added unit tests for caching demo.

---
+++ spec/demos/caching_spec.lua
@@ -0,0 +1,19 @@
+local request = require 'http.functional.request'
+local writer = require 'http.functional.response'
+local describe, it, assert = describe, it, assert
+
+local function test_cases(app)
+	assert.not_nil(app)
+
+	it('responds with a counter', function()
+        for i=1, 10 do
+            local w, req = writer.new(), request.new()
+            app(w, req)
+            assert.same('Counter = 1\n', w.buffer)
+        end
+	end)
+end
+
+describe('demos.web.caching', function()
+    test_cases(require 'demos.web.caching')
+end)
