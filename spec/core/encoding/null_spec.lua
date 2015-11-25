d17ad4a39529b416a4f2ef1137ce4a7ed1bdd358
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-11-25 21:41:03 +0200

    Added test cases for core encoding null.

--- spec/core/encoding/null_spec.lua
+++ spec/core/encoding/null_spec.lua
@@ -2,7 +2,10 @@ local null = require 'core.encoding.null'
 local describe, it, assert = describe, it, assert
 
 describe('core.encoding.null', function()
-	it('', function()
-		assert.not_nil(null)
+	it('no op', function()
+		assert.equals('x', null.encode('x'))
+		assert.equals('x', null.decode('x'))
+		assert.equals(1, null.encoded_len(1))
+		assert.equals(1, null.decoded_len(1))
 	end)
 end)
