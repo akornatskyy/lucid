da6e69e255eb49e24c16e13bb319087ad47428ab
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-11-27 21:48:05 +0200

    Added test cases for core encoding json.

--- spec/core/encoding/json_spec.lua
+++ spec/core/encoding/json_spec.lua
@@ -2,7 +2,27 @@ local json = require 'core.encoding.json'
 local describe, it, assert = describe, it, assert
 
 describe('core.encoding.json', function()
-	it('', function()
-		assert.not_nil(json)
-	end)
+    it('encode decode', function()
+        local t = {x = 1}
+        assert.same(t, json.decode(json.encode(t)))
+    end)
+
+    describe('not implemented', function()
+        it('raises an error', function()
+            local saved = require
+            _G['require'] = function(m)
+                if m == 'cjson' then
+                    return error()
+                end
+                return saved(m)
+            end
+            package.loaded['cjson'] = nil
+            package.loaded['core.encoding.json'] = nil
+            local j = require 'core.encoding.json'
+            assert.error(j.encode, 'json encode is not implemented')
+            assert.error(j.decode, 'json decode is not implemented')
+            package.loaded['core.encoding.json'] = nil
+            _G['require'] = saved
+        end)
+    end)
 end)
