8d665a11b9fd7cf6ba9319a3245cdc87f3d31bb0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-11-28 21:38:22 +0200

    Added test cases for web mixins auth cookie.

--- spec/web/mixins/authcookie_spec.lua
+++ spec/web/mixins/authcookie_spec.lua
@@ -1,8 +1,35 @@
 local authcookie = require 'web.mixins.authcookie'
+local request = require 'http.functional.request'
+local writer = require 'http.functional.response'
 local describe, it, assert = describe, it, assert
 
 describe('web.mixins.authcookie', function()
-	it('', function()
-		assert.not_nil(authcookie)
+	it('extends time left', function()
+		local c = setmetatable({
+            req = request.new {
+                options = {
+                    auth_cookie = {name = '_a'},
+                    ticket = {
+                        max_age = 15,
+                        decode = function(self, c)
+                            assert.equals('cookie', c)
+                            return '1', 5
+                        end,
+                        encode = function(self, p)
+                            assert.equals('1', p)
+                            return '2'
+                        end
+                    }
+                },
+                get_cookie = function()
+                    return 'cookie'
+                end
+            },
+            w = writer.new()
+        }, {__index=authcookie})
+        local p, time_left = c:parse_auth_cookie()
+        assert.equals('1', p)
+        assert.equals(5, time_left)
+        assert.equals('_a=2; HttpOnly', c.w.headers['Set-Cookie'])
 	end)
 end)
