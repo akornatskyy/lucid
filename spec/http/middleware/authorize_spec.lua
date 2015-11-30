09ba282162f40e7f52174dd57437267fcb656b58
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-11-30 21:55:18 +0200

    Added test cases for http middleware authorize.

--- spec/http/middleware/authorize_spec.lua
+++ spec/http/middleware/authorize_spec.lua
@@ -1,8 +1,33 @@
 local authorize = require 'http.middleware.authorize'
+local request = require 'http.functional.request'
+local writer = require 'http.functional.response'
 local describe, it, assert = describe, it, assert
 
 describe('http.middleware.authorize', function()
-	it('', function()
-		assert.not_nil(authorize)
+	it('extends time left', function()
+        local options = {
+            auth_cookie = {name = '_a'},
+            ticket = {
+                max_age = 15,
+                decode = function(self, c)
+                    assert.equals('cookie', c)
+                    return '1', 5
+                end,
+                encode = function(self, p)
+                    assert.equals('1', p)
+                    return '2'
+                end
+            }
+        }
+
+        local app = authorize(function() end, options)
+        local w = writer.new()
+        local req = request.new {
+            get_cookie = function()
+                return 'cookie'
+            end
+        }
+        app(w, req)
+        assert.equals('_a=2; Path=/; HttpOnly', w.headers['Set-Cookie'])
 	end)
 end)
