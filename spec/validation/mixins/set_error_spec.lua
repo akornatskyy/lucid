c1d844562398d8445a47eec7fb5d0b3474f6a114
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-03 20:44:48 +0200

    Introduced validation mixins set error spec.

---
+++ spec/validation/mixins/set_error_spec.lua
@@ -0,0 +1,8 @@
+local set_error = require 'validation.mixins.set_error'
+local describe, it, assert = describe, it, assert
+
+describe('validation.mixins.set_error', function()
+	it('', function()
+		assert.not_nil(set_error)
+	end)
+end)
