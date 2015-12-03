dc8fd3069a293fb1db9860c5b5561c3215e279b7
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-03 20:45:08 +0200

    Introduced validation mixins validation spec.

---
+++ spec/validation/mixins/validation_spec.lua
@@ -0,0 +1,8 @@
+local validation = require 'validation.mixins.validation'
+local describe, it, assert = describe, it, assert
+
+describe('validation.mixins.validation', function()
+	it('', function()
+		assert.not_nil(validation)
+	end)
+end)
