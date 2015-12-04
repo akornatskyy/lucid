aa11baab8316e59b104a0784b794199e9f44680e
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-04 22:32:38 +0200

    Added test cases for validation mixins set error.

--- spec/validation/mixins/set_error_spec.lua
+++ spec/validation/mixins/set_error_spec.lua
@@ -2,7 +2,15 @@ local set_error = require 'validation.mixins.set_error'
 local describe, it, assert = describe, it, assert
 
 describe('validation.mixins.set_error', function()
-	it('', function()
-		assert.not_nil(set_error)
+	it('sets general error message', function()
+		local m = setmetatable({errors={}}, {__index=set_error})
+        m:set_error('message')
+        assert.equals('message', m.errors.__ERROR__)
+	end)
+
+    it('sets field error message', function()
+		local m = setmetatable({errors={}}, {__index=set_error})
+        m:set_error('message', 'field')
+        assert.equals('message', m.errors.field)
 	end)
 end)
