d6d6ffaacf486845bb9f901a4f786a2e29beaa3a
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-05 22:37:32 +0200

    Added test cases for http adapters nginx hello world.

--- spec/http/adapters/nginx/helloworld_spec.lua
+++ spec/http/adapters/nginx/helloworld_spec.lua
@@ -3,6 +3,17 @@ local describe, it, assert = describe, it, assert
 
 describe('http.adapters.nginx.helloworld', function()
 	it('', function()
-		assert.not_nil(helloworld)
+        local called = 0
+        local ngx = {
+            print = function(msg)
+                called = called + 1
+                assert.equals('Hello World!\n', msg)
+            end,
+            eof = function()
+                called = called + 1
+            end
+        }
+		helloworld(ngx)
+        assert.equals(2, called)
 	end)
 end)
