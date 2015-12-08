9c95b4bdcdedff83c544cd609c48b7bb1242c957
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-08 22:15:55 +0200

    Added test cases fir http adapters nginx buffered.

--- spec/http/adapters/nginx/buffered_spec.lua
+++ spec/http/adapters/nginx/buffered_spec.lua
@@ -2,7 +2,35 @@ local buffered = require 'http.adapters.nginx.buffered'
 local describe, it, assert = describe, it, assert
 
 describe('http.adapters.nginx.buffered', function()
-	it('', function()
-		assert.not_nil(buffered)
+	it('supports buffered writer', function()
+        local eof_called = false
+        local ngx = {
+            header = {},
+            var = {
+                request_method = 'GET',
+                uri = '/'
+            },
+            print = function(c)
+                assert.same({'Hello ', 'World'}, c)
+            end,
+            eof = function()
+                eof_called = true
+            end
+        }
+        local app_called = false
+		local app = function(w, req)
+            app_called = true
+            w:write('Hello ')
+            w:write('World')
+            assert.same(ngx, w.ngx)
+            assert.same(ngx.header, w.headers)
+            assert.same(ngx, req.ngx)
+            assert.equals('GET', req.method)
+            assert.equals('/', req.path)
+        end
+        buffered(app)(ngx)
+        assert(app_called)
+        assert(eof_called)
+        assert.same({}, ngx.header)
 	end)
 end)
