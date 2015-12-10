9aff86b0f9f623b4af6717044c08d69a953f84ab
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-10 22:28:28 +0200

    Added test cases for http adapters nginx base response writer.

--- spec/http/adapters/nginx/base_spec.lua
+++ spec/http/adapters/nginx/base_spec.lua
@@ -2,7 +2,23 @@ local base = require 'http.adapters.nginx.base'
 local describe, it, assert = describe, it, assert
 
 describe('http.adapters.nginx.base', function()
-	it('', function()
-		assert.not_nil(base)
-	end)
+    describe('response writer', function()
+        it('get or set status code', function()
+            local w = setmetatable({ngx={}}, {__index=base.ResponseWriter})
+            assert.is_nil(w:get_status_code())
+            w:set_status_code(403)
+            assert.equals(403, w:get_status_code())
+        end)
+
+        it('flushes', function()
+            local flush_called = false
+            local w = setmetatable({ngx = {
+                flush = function()
+                    flush_called = true
+                end
+            }}, {__index=base.ResponseWriter})
+            w:flush()
+            assert(flush_called)
+        end)
+    end)
 end)
