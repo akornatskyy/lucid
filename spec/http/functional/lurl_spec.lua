05af5df1f750bbfda655e143173ddc7aa8112206
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-18 22:23:30 +0200

    Added test case for lurl usage.

--- spec/http/functional/lurl_spec.lua
+++ spec/http/functional/lurl_spec.lua
@@ -2,7 +2,14 @@ local lurl = require 'http.functional.lurl'
 local describe, it, assert = describe, it, assert
 
 describe('http.functional.lurl', function()
-	it('', function()
-		assert.not_nil(lurl)
+	it('shows usage', function()
+        local saved = print
+        local usage = nil
+        _G['print'] = function(s)
+            usage = s
+        end
+		lurl()
+        _G['print'] = saved
+        assert.not_nil(usage)
 	end)
 end)
