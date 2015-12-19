f4c8694f8252b81b5b1d035fbc42c4340126879d
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-12-19 21:26:27 +0200

    Added test case for lurl calls app by path.

--- spec/http/functional/lurl_spec.lua
+++ spec/http/functional/lurl_spec.lua
@@ -12,4 +12,29 @@ describe('http.functional.lurl', function()
         _G['print'] = saved
         assert.not_nil(usage)
 	end)
+
+    it('calls app by path', function()
+        local sarg = arg
+        local sio = io
+        local cases = {
+            'demos/http/hello.lua',
+            'demos.http.hello',
+            'demos/web/hello.lua',
+            'demos.http.hello'
+        }
+        for _, c in next, cases do
+            local write_called = false
+            _G['arg'] = {c, '/'}
+            _G['io'] = {
+                write = function(s)
+                    write_called = true
+                    assert.equals('Hello World!\n', s)
+                end
+            }
+            lurl()
+            assert(write_called)
+        end
+        _G['arg'] = sarg
+        _G['io'] = sio
+    end)
 end)
